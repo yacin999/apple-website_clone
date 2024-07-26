fa763fdc076e10d8b86f03059d9a0e7b9b683ba4
yacine <yacinomar999@gmail.com>
2024-07-26 21:33:35 +0100

    add video to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { videoSrc } from "../utils"
 
 const Hero = () => {
+
+    useGSAP(()=> {
+        gsap.to("#hero", {
+            opacity : 1,
+            delay : 1.5
+        })
+    }, [])
+
+
   return (
     <section className='w-full nav-height bg-black relative'>
         <div className='h-5/6 w-full flex-center flex-col'>
-            <p className='hero-title'>iPhone 15 Pro</p>
+            <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+            <div className='md:w-10/12 w-9/12'>
+                <video>
+                    <source src={videoSrc} />
+                </video>
+            </div>
         </div>
     </section>
   )

1608d71c24b7710e3d8ed0d295ce3e62c66bee83
yacine <yacinomar999@gmail.com>
2024-07-27 12:20:42 +0100

    add videos to video carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { hightlightsSlides } from '../constants'
 
 const VideoCarousel = () => {
   return (
-    <div>VideoCarousel</div>
+    <>
+        <div className='flex items-center'>
+            {hightlightsSlides.map((list, index) => (
+                <div 
+                    key={list.id}
+                    id='slider'
+                    className='sm:pr-20 pr-10'
+                >
+                    <div className='video-carousel_container'>
+                        <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
+                            <video
+                                id='video'
+                                playsInline={true}
+                                preload='auto'
+                                muted
+                            >
+                                <source src={list.video} type='video/mp4'/> 
+                            </video>
+                        </div>
+                    </div>    
+                </div>
+            ))}
+        </div>
+    </>
   )
 }
 

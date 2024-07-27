727dca0f63ddaa2c5c219869a7f93818934d2f27
yacine <yacinomar999@gmail.com>
2024-07-27 19:00:18 +0100

    create video courosel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { hightlightsSlides } from '../constants'
+import gsap from 'gsap'
 
 const VideoCarousel = () => {
+    const videoRef = useRef([])
+    const videoSpanRef = useRef([])
+    const videoDivRef = useRef([])
+
+
+    const [video, setVideo] = useState({
+        isEnd : false,
+        startPlay : false,
+        videoId : 0,
+        isLastVideo : false,
+        isPlaying : false
+    })
+    const [loadedData, setLoadedData] = useState([])
+
+    const {isEnd, isLastVideo, startPlay, videoId, isPlaying} = video
+
+    useEffect(() => {
+      
+        if (loadedData.length > 3) {
+            if (isPlaying) {
+                videoRef.current[videoId].pause()
+            }else {
+                startPlay && videoRef.current[videoId].play()
+            }
+        }
+      
+    }, [startPlay, videoId, isPlaying, loadedData])
+    
+
+    useEffect(()=> {
+        const currentProgress = 0
+        let span = videoSpanRef.current
+
+        if (span[videoId]) {
+            // animate the progress of the video
+            let anim = gsap.to(span[videoId], {
+                onUpdate : () => {
+
+                },
+                onComplete : () => {
+
+                }
+            })
+        }
+    }, [videoId, startPlay])
   return (
     <>
         <div className='flex items-center'>
-            {hightlightsSlides.map((list, index) => (
+            {hightlightsSlides.map((slide, index) => (
                 <div 
-                    key={list.id}
+                    key={slide.id}
                     id='slider'
                     className='sm:pr-20 pr-10'
                 >
@@ -18,13 +64,45 @@ const VideoCarousel = () => {
                                 playsInline={true}
                                 preload='auto'
                                 muted
+                                ref={(el)=> (videoRef.current[index] = el)}
+                                onPlay={() => {
+                                    setVideo((prevVideo)=>({
+                                        ...prevVideo,
+                                        isPlaying : true
+                                    }))
+                                }}
                             >
-                                <source src={list.video} type='video/mp4'/> 
+                                <source src={slide.video} type='video/mp4'/> 
                             </video>
+                        </div>
+
+                        <div className='absolute top-12 left-[5%] z-10'>
+                            {slide.textLists.map(text => (
+                                <p key={text} className='md:text-2xl text-xl font-medium'>{text}</p>
+                            ))}
                         </div>
                     </div>    
                 </div>
             ))}
+        </div>
+
+        <div className='relative flex-center mt-10'>
+            <div className='flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full'>
+                {videoRef.current.map((_, i) => (
+                    <span 
+                        key={i} 
+                        ref={(el)=> (videoDivRef.current[i] = el)}
+                        className='mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer'
+                    >
+                        <span 
+                            className='absolute h-full w-full rounded-full'
+                            ref={(el)=> (videoSpanRef.current[i] = el)}
+                        >
+                            
+                        </span>
+                    </span>
+                ))}
+            </div>
         </div>
     </>
   )

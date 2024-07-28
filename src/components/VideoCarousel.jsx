a2b37e7f7de5075e067b45e726ab7a196515f35b
yacine <yacinomar999@gmail.com>
2024-07-28 11:18:09 +0100

    register scroll trigger in video

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -3,6 +3,10 @@ import { hightlightsSlides } from '../constants'
 import gsap from 'gsap'
 import { pauseImg, playImg, replayImg } from '../utils'
 import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+
 
 const VideoCarousel = () => {
     const videoRef = useRef([])

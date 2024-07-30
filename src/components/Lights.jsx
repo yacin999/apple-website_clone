7d201bca27eb690f9c6c37f854dfd9a29c84bc81
yacine <yacinomar999@gmail.com>
2024-07-30 18:22:29 +0100

    create lights

---
+++ src/components/Lights.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lights = () => {
+  return (
+    <div>Lights</div>
+  )
+}
+
+export default Lights
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,26 @@
+import { PerspectiveCamera, View } from '@react-three/drei'
 import React from 'react'
 
-const ModelView = () => {
+const ModelView = ({
+    index, 
+    groupRef,
+    gsapType,
+    controlRef,
+    setRotationSize,
+    size,
+    item
+}) => {
   return (
-    <div>ModelView</div>
+    <View
+        index={index}
+        id={gsapType}
+        className={`border-2 border-red-500 w-full h-full ${index === 2 ? 'right-[-100%]' :  ''}`}
+
+    >
+        {/* Ambient light */}
+        <ambientLight intensity={0.3} />
+        <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+    </View>
   )
 }
 

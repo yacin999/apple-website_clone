f58aa7bfeb0c99edde05102f75641267cb76ac84
yacine <yacinomar999@gmail.com>
2024-07-31 19:55:50 +0100

    test sentry

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 
+import * as Sentry from "@sentry/react"
 
 function App() {
-
-  return (
-    <main className="bg-black">
-      <Navbar/>
-      <Hero/>
-      <Highlights/>
-      <Model/>
-    </main>
-  )
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+  // return (
+  //   <main className="bg-black">
+  //     <Navbar/>
+  //     <Hero/>
+  //     <Highlights/>
+  //     <Model/>
+  //   </main>
+  // )
 }
 
-export default App
+export default Sentry.withProfiler(App); 

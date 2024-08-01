4ee45f8d79a85f10cec4ad2d2ee1d974c5363753
yacine <yacinomar999@gmail.com>
2024-08-01 13:06:31 +0100

    create animateWithGsap animation function

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,22 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React from 'react'
 
 const Features = () => {
+    useGSAP(()=> {
+        gsap.to("#features_title", {
+            
+        })
+    }, [])
+
   return (
-    <div>Features</div>
+    <section className='h-full common-padding bg-zinc relative overflow-hidden'>
+        <div className='screen-max-width'>
+            <div className='mb-12 w-full'>
+                <h1 id='features_title'>Explore the full story.</h1>
+            </div>
+        </div>
+    </section>
   )
 }
 
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,21 @@
+
+
+
+
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+    gsap.to(target, {
+        ...animationProps,
+        scrollTrigger : {
+            trigger : target,
+            toggleActions : "restart reverse restart reverse",
+            start : 'top 85%',
+            ...scrollProps
+        }
+    })
+}
+
+
 export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
     timeline.to(rotationRef.current.rotation, {
         y : rotationState,

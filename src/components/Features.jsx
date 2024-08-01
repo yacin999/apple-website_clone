688ad8e10a0c0b953768b9b12abce7dbc13133fd
yacine <yacinomar999@gmail.com>
2024-08-01 18:21:48 +0100

    add freatures

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -16,6 +16,13 @@ const Features = () => {
             <div className='mb-12 w-full'>
                 <h1 id='features_title' className='section-heading'>Explore the full story.</h1>
             </div>
+
+            <div className='flex flex-col justify-center items-center overflow-hidden'>
+                <div className='mt-32 mb-24 pl-24'>
+                    <h2 className='text-5xl lg:text-7xl font-semibold'>iPhone.</h2>
+                    <h2 className='text-5xl lg:text-7xl font-semibold'>Forged in Titanium.</h2>
+                </div>
+            </div>
         </div>
     </section>
   )

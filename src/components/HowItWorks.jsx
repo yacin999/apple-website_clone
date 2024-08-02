83422dd54d9e5d1f25c43da262e02fbc92e9d496
yacine <yacinomar999@gmail.com>
2024-08-02 18:19:18 +0100

    add video to chip

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import { chipImg } from '../utils'
+import React, { useRef } from 'react'
+import { chipImg, frameImg, frameVideo } from '../utils'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const HowItWorks = () => {
+  const videoRef = useRef()
+
+  useGSAP(()=> {
+    gsap.from('#chip', {
+      scrollTrigger : {
+        trigger : "#chip",
+        start : "20% bottom"
+      },
+      opacity : 0,
+      scale : 2,
+      duration : 2,
+      ease : "power2.inOut"
+    })
+  }, [])
+
+
   return (
     <section className='common-padding'>
       <div className='screen-max-width'>
         <div id='chip' className='flex-center w-full my-20'>
           <img src={chipImg} alt='chip' width={180} height={180}/>
+        </div>
+
+        <div className='flex flex-col items-center'>
+          <h2 className='hiw-title'>
+            A17 Pro chip.
+            <br/> A monstor win for gamming.
+          </h2>
+
+          <p className='hiw-subtitle'>
+            It's here. The biggest redesign in the history of Apple GPUs.
+          </p>
+        </div>
+
+        <div className='mt-10 md:mt-20 mb-14'>
+          <div className='relative h-full flex-center'>
+            <div className='overflow-hidden'>
+              <img src={frameImg} alt='frame' className='bg-transparent relative z-10' />
+            </div>
+
+            <div className='hiw-video'>
+                <video 
+                  className='pointer-events-none' 
+                  playsInline
+                  autoPlay
+                  preload='none'
+                  muted
+                  ref={videoRef}
+                >
+                  <source  src={frameVideo} type='video/mp4'/>
+                </video>
+              </div>
+          </div>
         </div>
       </div>
     </section>

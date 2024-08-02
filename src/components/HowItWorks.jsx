f6cda705a85923c2b921a7f345973794a1a9654f
yacine <yacinomar999@gmail.com>
2024-08-02 18:38:33 +0100

    add two paragraphs to howitworks

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -57,6 +57,34 @@ const HowItWorks = () => {
                 </video>
               </div>
           </div>
+
+          <p className='text-gray font-semibold text-center mt-3'>Honkai: Star Rail</p>
+
+          
+          <div className='hiw-text-container'>  
+            <div className='flex flex-1 justify-center flex-col'>
+                <p className='hiw-text g_fadeIn'>
+                    A17 Pro is an entirely new class of iPhone chip that delevers our space {" "}
+                    <span className='text-white'>
+                        best graphic performance by far
+                    </span>
+                </p>
+            </div>
+
+            <p className='hiw-text g_fadeIn'>
+                Mobile {" "}
+                <span className='text-white'>
+                    games will look and feel so immersive
+                </span>
+                with incredibly detailed environments and characters.                                   
+            </p>
+          </div>
+
+          <div className='flex-1 flex justice flex-col g_fadeIn'>
+            <p className='hiw-text'>New</p>
+            <p className='hiw-bigtext'>Pro-class GPU</p>
+            <p className='hiw-text'>with 6 cores</p>
+          </div>
         </div>
       </div>
     </section>

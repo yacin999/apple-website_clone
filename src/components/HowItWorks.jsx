5682d06b27b42faa4fcf92772d07022a2705cf04
yacine <yacinomar999@gmail.com>
2024-08-02 11:57:31 +0100

    add chip img to howitworks

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { chipImg } from '../utils'
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    <section className='common-padding'>
+      <div className='screen-max-width'>
+        <div id='chip' className='flex-center w-full my-20'>
+          <img src={chipImg} alt='chip' width={180} height={180}/>
+        </div>
+      </div>
+    </section>
   )
 }
 
